16a2fcafa89cd21fa5ba0de45bd11b0adb8d9849
TotalEntropy <26179509+TotalEntropy@users.noreply.github.com>
2019-04-26 13:19:57 -0400

    Redo file structure

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from bin/scrape_mars import scrape
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+
+@app.route('/')
+def home():
+
+    mars_info = mongo.db.mars_info.find_one()
+
+    return render_template('/bin/index.html', mars_info=mars_info)
+
+@app.route('/scrape_new')
+def scrape_new ():
+    mars_info = mongo.db.mars_info
+    mars_info.update({}, scrape(), upsert=True)
+    return redirect('/')
+
+if __name__ == "__main__": 
+    app.run(debug= True)
