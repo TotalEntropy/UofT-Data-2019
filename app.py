0c173a6137676cf1b3a4eaa055918957b7674dea
TotalEntropy <26179509+TotalEntropy@users.noreply.github.com>
2019-04-26 13:37:30 -0400

    Feature complete
    TODO At certain pixel widths the table will overlap with the hemisphere images

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
 
-@app.route('/')
+@app.route("/")
 def home():
 
     mars_info = mongo.db.mars_info.find_one()
 
-    return render_template('index.html', mars_info=mars_info)
+    return render_template("index.html", mars_info=mars_info)
 
 @app.route('/scrape_new')
 def scrape_new ():
     mars_info = mongo.db.mars_info
     mars_info.update({}, scrape(), upsert=True)
-    return redirect('/')
+    return redirect("/")
 
 if __name__ == "__main__": 
     app.run(debug= True)
